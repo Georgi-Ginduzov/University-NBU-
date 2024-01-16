279d5af9aad8257148083d96ba0e2b431ff360e6
Georgi <123997331+bunnyhype@users.noreply.github.com>
2024-01-16 16:01:57 +0200

    Perfectly Balanced Tree

---
+++ Data Structures/Trees/AVLTrees/StartUp.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+typedef int DataT;
+typedef struct node* po;
+
+struct node {
+	DataT data;
+	po left;
+	po right;
+};
+
+po ibd(int n) {
+	po darj;
+	DataT x;
+
+	if (n > 0)
+	{
+		int nl = n / 2, nd = n - nl - 1;
+		darj = new node;
+		std::cin >> x;
+		darj->data = x;
+		darj->left = ibd(nl);
+		darj->right = ibd(nd);
+		return darj;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+int main() {
+	int n;
+	po root;
+	std::cin >> n;
+	root = ibd(n);
+}
