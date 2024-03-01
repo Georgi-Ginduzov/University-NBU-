d12dd7491b8502fd720d0154857e3c94f9f43882
Georgi <georgi.ginduzov17d@gmail.com>
2024-03-01 11:32:29 +0200

    Refactorings

---
+++ Algorithms Advanced/AlgorithmAnalisys/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var map = new Dictionary<int, int>();
+            int randomNumber = int.Parse(Console.ReadLine());
+            //random number
+            for (int i = 0; i < n; i++)
+            {
+                int k = 0;
+                map[i] = k++;
+            }
+
+            for (int i = 0; ; i++)
+            {
+                Console.WriteLine(map[i]);
+            }
+
+            
+        }
+    }
+}
