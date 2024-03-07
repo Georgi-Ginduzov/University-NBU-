aa0bedd474b9adda1524ea3b0165f3189d46bbe2
Georgi <georgi.ginduzov17d@gmail.com>
2024-03-07 11:35:19 +0200

    Calculate years of experience in a company

---
+++ Java Programming/Task 3/src/Main.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        int yearOfHiring = input.nextInt();
+        int monthOfHiring = input.nextInt();
+        int dayOfHiring = input.nextInt();
+        int yearOfLeaving = input.nextInt();
+        int monthOfLeaving = input.nextInt();
+        int dayOfLeaving = input.nextInt();
+
+        Manager manager = new Manager(name);
+        LocalDate hiringDate = LocalDate.of(yearOfHiring, monthOfHiring, dayOfHiring);
+        LocalDate leavingDate = LocalDate.of(yearOfLeaving, monthOfLeaving, dayOfLeaving);
+        manager.setDateOfHire(hiringDate, leavingDate);
+
+        System.out.print(manager.getDateOfHire());
+        System.out.print("\n" + manager.numberOfYearsInCompanyTill(leavingDate));
+
+    }
+}
